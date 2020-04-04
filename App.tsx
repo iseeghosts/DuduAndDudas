9e8d8655d847b824c41f65485f6692dbb698c8f7
aadarshashutosh <aadarshashutosh@outlook.com>
2020-04-04 20:44:40 +0530

    updates

--- App.tsx
+++ App.tsx
@@ -1,38 +1,57 @@
-import React from 'react';
-import { StyleSheet, Button, Text, View, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, TextInput, Button, StyleSheet, ScrollView} from 'react-native';
 
-function Mursi(props: { dudu: React.ReactNode; }) {
-  return(
-    <View style={styler.secondaryContainer}>
-      <Text style={styler.secText}>
-        Well Hello There {props.dudu}!
-      </Text>
-    </View>
-  )
+function newApp() {
+    const [count, setCount] = useState(0);
+    return (
+        <ScrollView style={styler.mainContainer}>
+            <Text style={{textAlign:'center', textAlignVertical:'center'}}>
+                Starts Here!
+            </Text>
+            <View style={styler.firstContainer}>
+                <Text>
+                    You clicked {count} times!
+                </Text>
+                <Button onPress={() => setCount(count+1)} title={'Click It!'} />
+            </View>
+            <View style={styler.secContainer}>
+                <Text>
+                    Part 2 is here!
+                </Text>
+                <TextInput style={styler.textInput} placeholder="Type Into Me!" />
+            </View>
+        </ScrollView>
+    );
 }
-export default function suraj() {
-  return(
-    <View style={styler.mainContainer}>
-      <Text>This is a sample web app.</Text>
-      <Mursi dudu='LuluLala' />
-    </View>
-  );
-}
+export default newApp;
 
 const styler = StyleSheet.create({
-  mainContainer: {
-    flex:2,
-    justifyContent:'space-evenly',
-    alignItems:'center',
-    backgroundColor:'grey'
-  },
-  secondaryContainer:
-  {
-    justifyContent:'space-evenly',
-    alignItems:'center',
-    backgroundColor:'black',
-  },
-  secText: {
-    color:'#fff'
-  }
+    mainContainer: {
+
+    },
+    firstContainer: {
+        backgroundColor:'#fff',
+        flex:1,
+        justifyContent:'space-evenly',
+        alignItems:'center'
+    },
+    secContainer: {
+        backgroundColor:'#fff',
+        flex:1,
+        justifyContent:'space-evenly',
+        alignItems:'center'
+    },
+    textInput: {
+        color:'black',
+        fontWeight:'bold',
+        backgroundColor:'#555',
+        height:25,
+        borderColor: 'black',
+        // borderWidth: 0.1,
+        // borderRadius: 2.5,
+        padding:10,
+        textAlign:'center',
+        shadowOpacity:0.5,
+        shadowOffset:{width:-2, height:-2},
+    }
 })
